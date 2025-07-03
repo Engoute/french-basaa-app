f854f7d223dc941c8e0d3fc443d30b361a3990aa
Engoute <64826422+Engoute@users.noreply.github.com>
2025-07-03 15:19:10 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="french_basaa_app",
+    version="0.1.0",
+    packages=find_packages(),
+    # This will find your 'app' and 'orpheus' code
+)
